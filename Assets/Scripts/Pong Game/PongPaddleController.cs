6029ead2c14186672af64996a6bb6856a334dca9
Jonas Mack <jonas.mack.hhg@gmail.com>
2023-11-21 13:22:33 +0100

    fixed paddle flipping when rotating to 180 or -180 degrees

--- Assets/Scripts/Pong Game/PongPaddleController.cs
+++ Assets/Scripts/Pong Game/PongPaddleController.cs
@@ -7,6 +7,7 @@ public class PongPaddleController : MonoBehaviour
 {
     public GameObject PlayerInGamePosition;
     public float MaxPositionDisplacement = 100f;
+    public float MaxZAngleDifference = 100f;
 
     Rigidbody rb;
 
@@ -39,9 +40,38 @@ public class PongPaddleController : MonoBehaviour
 
     void RotateToPlayerRotation()
     {
-        // if the displacement is too large the rotation should be changed instantly using the transform just like in MovePlayerPosition but it doesnt seem to work right
-        float _zAngleDifference = PlayerInGamePosition.transform.rotation.z - transform.rotation.z;
-        float _angularVelocity = _zAngleDifference / Time.fixedDeltaTime;
-        rb.angularVelocity = new Vector3(transform.rotation.x, transform.rotation.y, _angularVelocity);
+        float _zAngleDifference = PlayerInGamePosition.transform.eulerAngles.z - transform.eulerAngles.z;
+
+        if (Mathf.Abs(_zAngleDifference) > MaxZAngleDifference)
+        {
+            float _targetZRotation = PlayerInGamePosition.transform.eulerAngles.z;
+
+            if (_targetZRotation > 180f)
+            {
+                while (_targetZRotation > 180f)
+                {
+                    _targetZRotation -= 180f;
+                }
+
+                _targetZRotation -= 180f;
+            }
+            else if (_targetZRotation < -180f)
+            {
+                while (_targetZRotation < -180f)
+                {
+                    _targetZRotation += 180f;
+                }
+
+                _targetZRotation += 180f;
+            }
+
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, _targetZRotation);
+        }
+        else
+        {
+            float _zAngleDifferenceRadians = _zAngleDifference * Mathf.Deg2Rad;
+            float _zAngularVelocity = _zAngleDifferenceRadians / Time.fixedDeltaTime;
+            rb.angularVelocity = new Vector3(transform.rotation.x, transform.rotation.y, _zAngularVelocity);
+        }
     }
 }
