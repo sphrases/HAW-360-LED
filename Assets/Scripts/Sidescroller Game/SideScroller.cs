fa9028c4dd385f3077e4045e6205740a39025225
Jonas Mack <jonas.mack.hhg@gmail.com>
2023-12-01 01:14:31 +0100

    added to sidescroller game

--- Assets/Scripts/Sidescroller Game/SideScroller.cs
+++ Assets/Scripts/Sidescroller Game/SideScroller.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class SideScroller : MonoBehaviour
 {
+    public float XPositionBounds = 9.6f;
     public float ScrollSpeed = -4f;
 
     private void Update()
     {
         transform.Translate(ScrollSpeed * Time.deltaTime, 0f, 0f);
+        CheckXPosition();
+    }
+
+    void CheckXPosition()
+    {
+        if (transform.position.x < -XPositionBounds)
+        {
+            transform.position = new Vector3(XPositionBounds, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > XPositionBounds)
+        {
+            transform.position = new Vector3(-XPositionBounds, transform.position.y, transform.position.z);
+        }
     }
 }
