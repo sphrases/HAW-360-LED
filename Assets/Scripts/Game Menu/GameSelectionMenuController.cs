68074acf83e5b5112c139c4edfc8fd1a4bfb4683
A. Benjamins <deraikoben@gmail.com>
2023-12-18 12:54:37 +0100

    Changed to wrapped Menu stucture

--- Assets/Scripts/Game Menu/GameSelectionMenuController.cs
+++ Assets/Scripts/Game Menu/GameSelectionMenuController.cs
@@ -12,22 +12,33 @@ public class GameSelectionMenuController : MonoBehaviour
     // public GameMenuButtonHandler PlayShooterGameButton;
     // public GameMenuButtonHandler PlayJumpGameButton;
 
+    public RectTransform CalculationCanvas;
     public event System.Action<GameManager.Games> StartGame;
-
-
-
+    public float offsetYAxis = 0;
     public List<GameBaseClass> gameBaseClasses;
-    private float offsetXAxis = 4.36f;
+
+
+
+    private float _canvasHeightNormalized;
+    private float _canvasWidthNormalized;
+    private float _offsetYAxisPadding = 2.5f;
+    private float _offsetYAxisElementHeight = 1;
 
     void Awake()
     {
 
+        _canvasHeightNormalized = CalculationCanvas.rect.height / 2;
+        _canvasWidthNormalized = CalculationCanvas.rect.width / 2;
+
+        // Debug.Log("_canvasHeigh: " + _canvasHeightNormalized + "   width: " + _canvasWidthNormalized);
+
         foreach (GameBaseClass gClass in gameBaseClasses)
         {
             Transform positionOffset = this.transform;
-            positionOffset.position = new Vector3(this.transform.position.x + offsetXAxis, this.transform.position.y, this.transform.position.z);
-            Debug.Log(positionOffset.position);
+            positionOffset.position = new Vector3(this.transform.position.x, _canvasHeightNormalized - offsetYAxis - _offsetYAxisElementHeight, this.transform.position.z);
+            // Debug.Log(positionOffset.position);
             Instantiate(gClass.gameTitleCard, positionOffset.position, positionOffset.rotation);
+            offsetYAxis += _offsetYAxisPadding;
         }
 
 
