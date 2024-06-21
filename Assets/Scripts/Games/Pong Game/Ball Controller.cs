a5a150671230164fceb8e6c2e0c55df1ac2a6602
jonasmack0 <jonas.mack.hhg@gmail.com>
2024-06-21 14:21:08 +0200

    fixed pong trails glitching on teleport

--- Assets/Scripts/Games/Pong Game/Ball Controller.cs
+++ Assets/Scripts/Games/Pong Game/Ball Controller.cs
@@ -6,6 +6,7 @@ public class BallMovementController : MonoBehaviour
 {
     public float XPositionBounds = 9.6f;
     public float MaxVelocity = 1.0f;
+    public List<ParticleSystem> particles = new List<ParticleSystem>();
 
     Rigidbody rb;
 
@@ -25,11 +26,15 @@ public class BallMovementController : MonoBehaviour
     {
         if(transform.position.x < -XPositionBounds)
         {
+            PauseParticles();
             transform.position = new Vector3(XPositionBounds, transform.position.y, transform.position.z);
+            Invoke("ContinueParticles", 0.01f);
         }
         else if(transform.position.x > XPositionBounds)
         {
+            PauseParticles();
             transform.position = new Vector3(-XPositionBounds, transform.position.y, transform.position.z);
+            Invoke("ContinueParticles", 0.01f);
         }
     }
 
@@ -38,6 +43,23 @@ public class BallMovementController : MonoBehaviour
         if(rb.velocity.magnitude > MaxVelocity)
         {
             rb.velocity = Vector3.Lerp(rb.velocity, rb.velocity.normalized * MaxVelocity, 0.5f);
+        }
+    }
+
+    void PauseParticles()
+    {
+        foreach(ParticleSystem particle in particles)
+        {
+            particle.Stop();
+            particle.Clear();
+        }
+    }
+
+    void ContinueParticles()
+    {
+        foreach (ParticleSystem particle in particles)
+        {
+            particle.Play();
         }
     }
 }
