c1fc0bf196bd09826d050cb383380f1da2552dad
jonasmack0 <jonas.mack.hhg@gmail.com>
2024-06-21 13:43:00 +0200

    implemented pong trails and changed bounce threshold

--- Assets/Scripts/Games/Pong Game/PongBallCollisionHandler.cs
+++ Assets/Scripts/Games/Pong Game/PongBallCollisionHandler.cs
@@ -7,6 +7,8 @@ public class PongBallCollisionHandler : MonoBehaviour
     MeshRenderer meshRenderer;
     public GameObject GoalLine;
     MeshRenderer goalLineMeshRender;
+    public GameObject RightControllerRedTrail;
+    public GameObject LeftControllerBlueTrail;
 
     private void Start()
     {
@@ -20,6 +22,9 @@ public class PongBallCollisionHandler : MonoBehaviour
         {
             if (collision.transform.parent.GetComponentInChildren<FlatscreenPlayerTransformHandler>().ThisPlayersController.CompareTag("CustomGameControllerLeft"))
             {
+                LeftControllerBlueTrail.SetActive(true);
+                RightControllerRedTrail.SetActive(false);
+
                 GoalLine.SetActive(true);
                 GoalLine.GetComponent<PongGoalLineController>().LastControllerToHit = 1;
                 GoalLine.GetComponent<PongGoalLineController>().LastCollisionWasFirstCollsion = true;
@@ -30,6 +35,9 @@ public class PongBallCollisionHandler : MonoBehaviour
             }
             else if (collision.transform.parent.GetComponentInChildren<FlatscreenPlayerTransformHandler>().ThisPlayersController.CompareTag("CustomGameControllerRight"))
             {
+                LeftControllerBlueTrail.SetActive(false);
+                RightControllerRedTrail.SetActive(true);
+
                 GoalLine.SetActive(true);
                 GoalLine.GetComponent<PongGoalLineController>().LastControllerToHit = 2;
                 GoalLine.GetComponent<PongGoalLineController>().LastCollisionWasFirstCollsion = true;
